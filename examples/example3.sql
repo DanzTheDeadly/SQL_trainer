b63240c032ea65854d19eaa351dda9b39318b6d0
DanzTheDeadly <danzerr@gmail.com>
2024-03-15 11:09:35 +0300

    Add example 3

--- examples/example3.sql
+++ examples/example3.sql
@@ -1,1 +1,27 @@
-SELECT 3
+SELECT
+  city
+, name
+, total
+, pos
+FROM (
+  SELECT
+    city
+  , name
+  , total
+  , row_number() OVER (PARTITION BY city ORDER BY total DESC) AS pos
+  FROM (
+    SELECT
+      city
+    , name
+    , total
+    FROM (
+      SELECT
+        city
+      , name
+      , count(user_id) as total
+      FROM users
+      GROUP BY city, name
+    ) t1
+  ) t2
+) t3
+WHERE pos <= 3
